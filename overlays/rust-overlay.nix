dd9bf48485344c0cb1fb7a8ed3c60551d7eb86fa
Dan Sully <dsully@users.noreply.github.com>
2025-04-13 10:58:32 -0700

    fix: try rust-overlay

---
+++ overlays/rust-overlay.nix
@@ -0,0 +1,12 @@
+let
+  rust-overlay = import (builtins.fetchGit {
+    url = "https://github.com/oxalica/rust-overlay.git";
+    rev = "fd76032706405b6abe09bff25ca7e28ac1fa0043";
+  });
+in [
+  rust-overlay
+  (self: super: {
+    rustc = self.rust-bin.stable.latest.default;
+    cargo = self.rust-bin.stable.latest.default;
+  })
+]
