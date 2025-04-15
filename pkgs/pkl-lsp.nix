1dd073279b36e1707806c4557ed2428114a2a5b9
Dan Sully <dsully@users.noreply.github.com>
2025-04-14 18:51:44 -0700

    feat: update

--- pkgs/pkl-lsp.nix
+++ pkgs/pkl-lsp.nix
@@ -1,29 +1,36 @@
 {
   lib,
-  pkgs,
   stdenv,
-  fetchFromGitHub,
+  fetchurl,
+  jdk23_headless,
+  makeWrapper,
 }:
 stdenv.mkDerivation rec {
   pname = "pkl-lsp";
   version = "0.2.0";
 
-  src = fetchFromGitHub {
-    owner = "apple";
-    repo = "pkl-lsp";
-    rev = version;
-    hash = "sha256-fB8HvA2jOI1xHWq3/XYyTQJTNn4ixY7fFvqw3FJI4Ac=";
-  };
-
-  buildInputs = [
-    pkgs.kotlin
-  ];
-
   meta = {
     description = "Language server for Pkl, implementing the server-side of the Language Server Protocol";
     homepage = "https://github.com/apple/pkl-lsp";
     license = lib.licenses.asl20;
-    mainProgram = "pkl-lsp";
     platforms = lib.platforms.all;
   };
+
+  src = fetchurl {
+    url = "https://github.com/apple/pkl-lsp/releases/download/${version}/${pname}-${version}.jar";
+    sha256 = "sha256-1sdiwrqapyivp4q4qda2qs6spmch2kxvn94ha35msj6d1jxf6jsz";
+  };
+
+  nativeBuildInputs = [makeWrapper];
+  buildInputs = [jdk23_headless];
+
+  dontUnpack = true;
+
+  installPhase = ''
+    mkdir -p $out/share/java $out/bin
+    cp $src $out/share/java/${pname}-${version}.jar
+
+    makeWrapper ${jdk23_headless}/bin/java $out/bin/${pname} \
+      --add-flags "-jar $out/share/java/${pname}-${version}.jar"
+  '';
 }
