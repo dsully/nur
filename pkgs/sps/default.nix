051212376e6eab9451cf6c1fdf123304e719f6c2
Dan Sully <dsully@users.noreply.github.com>
2025-05-03 10:34:42 -0700

    fix: don't build sps on linux

--- pkgs/sps/default.nix
+++ pkgs/sps/default.nix
@@ -5,46 +5,44 @@
   pkgs,
   stdenv,
 }:
-rustPlatform.buildRustPackage rec {
-  pname = "sps";
-  version = "2025-05-01-255e633";
+lib.optionalAttrs stdenv.isDarwin (
+  rustPlatform.buildRustPackage rec {
+    pname = "sps";
+    version = "2025-05-01-255e633";
 
-  src = fetchFromGitHub {
-    owner = "alexykn";
-    repo = "sps";
-    rev = "255e633f1506689d1b176ab4a8d60b928096dc03";
-    hash = "sha256-vd0KCGM7tOpYe0L4DukIIx6/RKzM3JsJzIj2FIizSeg=";
-  };
+    src = fetchFromGitHub {
+      owner = "alexykn";
+      repo = "sps";
+      rev = "255e633f1506689d1b176ab4a8d60b928096dc03";
+      hash = "sha256-vd0KCGM7tOpYe0L4DukIIx6/RKzM3JsJzIj2FIizSeg=";
+    };
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    allowBuiltinFetchGit = true;
-  };
+    cargoLock = {
+      lockFile = ./Cargo.lock;
+      allowBuiltinFetchGit = true;
+    };
 
-  postPatch = ''
-    ln -s ${./Cargo.lock} Cargo.lock
-  '';
+    postPatch = ''
+      ln -s ${./Cargo.lock} Cargo.lock
+    '';
 
-  doCheck = false;
+    doCheck = false;
 
-  nativeBuildInputs = [
-    pkgs.pkg-config
-  ];
-
-  buildInputs =
-    [
-      pkgs.openssl
-    ]
-    ++ lib.optionals stdenv.isLinux [
-      pkgs.gtk3
-      pkgs.glib
+    nativeBuildInputs = [
+      pkgs.pkg-config
     ];
 
-  meta = {
-    description = "Rust based package manager for macOS";
-    homepage = "https://github.com/alexykn/sps";
-    license = lib.licenses.bsd3;
-    maintainers = with lib.maintainers; [];
-    mainProgram = "sps";
-  };
-}
+    buildInputs = [
+      pkgs.openssl
+    ];
+
+    meta = {
+      description = "Rust based package manager for macOS";
+      homepage = "https://github.com/alexykn/sps";
+      maintainers = with lib.maintainers; [];
+      license = lib.licenses.bsd3;
+      mainProgram = "sps";
+      platforms = lib.platforms.darwin;
+    };
+  }
+)
