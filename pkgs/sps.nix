eb898b394d0e40d93856d1b99856d62a6dea9e18
Dan Sully <dsully@users.noreply.github.com>
2025-05-18 12:54:40 -0700

    chore: sps, lock file

--- pkgs/sps.nix
+++ pkgs/sps.nix
@@ -9,16 +9,16 @@ if stdenv.isDarwin
 then
   rustPlatform.buildRustPackage {
     pname = "sps";
-    version = "0.1.31";
+    version = "0.1.41";
 
     src = fetchFromGitHub {
       owner = "alexykn";
       repo = "sps";
-      rev = "0e015e481c21307cc3509693168e14698376c32e";
-      hash = "sha256-768aJMnDjPyDzztUK53MTCqqMVafhrqG/Bn2IyaxrFk=";
+      rev = "ee9848110c00e01148a9ef58e9746f2919083aee";
+      hash = "sha256-bCt89dwy8vUQF+hkvuX7/7sQWMfAQbEbGVjYCHXA6lg=";
     };
 
-    cargoHash = "sha256-w1gAXGxnx1cFHa8QO+Som983eQ31wvQwjFEHD9LqTcM=";
+    cargoHash = "sha256-JftKB7YA3ENwo2AftOgMoc0T91gPRZARY/CK9vTKScE=";
     useFetchCargoVendor = true;
     doCheck = false;
 
