2cc3268baa07e93fbb79718d7d635e4e26608709
Dan Sully <dsully@users.noreply.github.com>
2025-05-19 07:49:09 -0700

    fix: oli dependencies

--- pkgs/oli.nix
+++ pkgs/oli.nix
@@ -1,7 +1,8 @@
 {
-  rustPlatform,
-  lib,
   fetchFromGitHub,
+  lib,
+  pkgs,
+  rustPlatform,
 }:
 rustPlatform.buildRustPackage {
   pname = "oli";
@@ -10,13 +11,21 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "amrit110";
     repo = "oli";
-    rev = "30c2af6adf950abc446e4b23cce8c8847c76a95e";
-    hash = "sha256-0bRkvHzXZ9mUXZJG+Ekt1XZmD8BTTr7yBJdxAS3OmKA=";
+    rev = "7aff185dc42091633dd63da142c5deaa4b7996db";
+    hash = "sha256-mhGdv577EzkQ2Z44iR+qsIBLYn9mhTWS8HL5ZUqZFB4=";
   };
 
-  cargoHash = "sha256-aNdG3olJxgA1YvK+MXPlI66wvWTOuIgGJWjV1kGWuTQ=";
+  cargoHash = "sha256-KP0rmrU+RUmyK1HGf/QMI8ZJpL/1WTaEu737q9dx0Rc=";
   useFetchCargoVendor = true;
   doCheck = false;
+
+  nativeBuildInputs = [
+    pkgs.pkg-config
+  ];
+
+  buildInputs = [
+    pkgs.openssl
+  ];
 
   meta = {
     description = "A simple, fast terminal based AI coding assistant";
